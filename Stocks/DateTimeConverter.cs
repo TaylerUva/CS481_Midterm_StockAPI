d2ecfd6cfa20cb8afb54ebcbc95e69c81b20078e
Tayler Uva <tayleruva@gmail.com>
2019-03-12 23:46:51 -0700

    converted string to date

---
+++ Stocks/DateTimeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Stocks {
+    public class DateTimeConverter : IValueConverter {
+        public object Convert(object value,
+                           Type targetType,
+                           object parameter,
+                           CultureInfo culture) {
+            if (value != null) {
+                return DateTime.Parse(value.ToString());
+            } else {
+                return String.Empty;
+            }
+        }
+
+        public object ConvertBack(object value,
+                                  Type targetType,
+                                  object parameter,
+                                  CultureInfo culture) {
+            return DateTime.Parse(value.ToString());
+        }
+    }
+
+}
